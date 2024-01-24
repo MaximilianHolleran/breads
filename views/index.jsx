1efde3f299ccf9eb317203f5aceb841e2da3ef7f
MaximilianHolleran <mhholleran@gmail.com>
2024-01-23 19:46:04 -0500

    created new bread route

--- views/index.jsx
+++ views/index.jsx
@@ -4,6 +4,7 @@ const Default = require('./layouts/Default')
 function Index({breads, title}) {
     return (
       <Default title={title}>
+        
         <h2>Index Page</h2>
         <ul>
             {
@@ -18,6 +19,10 @@ function Index({breads, title}) {
                 })
             }
         </ul>
+        <div className="newButton">
+            <a href="/breads/new"><button>Add a new bread</button></a>
+        </div>
+
       </Default>
     )
 }
